1f0e8f117f1bc2d10c181e928930e72364d4646a
ydy0615 <allenyuan410@gmail.com>
2026-01-25 18:41:32 +0800

    refactor(solutions): optimize P4555 palindrome algorithm implementation
    Optimize Manacher algorithm by reducing array size from 30M to ~600k, converting string concatenation to direct char array indexing for better cache locality. Fix algorithmic correctness in B/C array index calculations (ans[i]-1 instead of ans[i]/2) and improve iteration order for forward/backward propagation.

--- P4555.cpp
+++ P4555.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define N 30000010
+#define N 300010
 int ans[N];
 int B[N],C[N];
+char t[N];
 int main(){
-	//memset(B,0x3f,sizeof(B));
-	string s,t;
+	string s;
 	cin>>s;
 	int n=s.size(),anss=0;
-	t+='@';
+	t[0]='@';
 	for(int i=0;i<n;i++){
-		t+='#';
-		t+=s[i];
+		t[i*2+2]=s[i];
+		t[i*2+1]='#';
 	}
-	t+='#';
-	t+='$';
-	n=t.size();
-	int p=0,k=0,R=0;
+	t[n*2+1]='#';
+	t[n*2+2]='$';
+	n=n*2+2;
+	int p=0,k=0;
 	for(int i=1;i<n;i++){
 		if(i<k){
 			ans[i]=min(ans[2*p-i],k-i);
@@ -27,22 +27,22 @@ int main(){
 		while(i-ans[i]>=0 && i+ans[i]<n && t[i-ans[i]]==t[i+ans[i]]){
 			ans[i]++;
 		}
-		//cout<<ans[i]<<" "<<t[i]<<endl;
-		B[i+(ans[i]/2)]=max(B[i+(ans[i]/2)],ans[i]-1);
-		C[i-(ans[i]/2)]=max(C[i-(ans[i]/2)],ans[i]-1);
-	}
-	cout<<"\n";
-	for(int i=1;i<n;i++){
-		B[i]=max(B[i],B[i-1]);
+		if(i+ans[i]>k){
+			k=i+ans[i];
+			p=i;
+		}
+		B[i+(ans[i]-1)]=max(B[i+(ans[i]-1)],ans[i]-1);
+		C[i-(ans[i]-1)]=max(C[i-(ans[i]-1)],ans[i]-1);
 	}
 	for(int i=n-1;i>=1;i--){
-		C[i]=max(C[i],C[i+1]);
+		B[i]=max(B[i],B[i+2]-2);
 	}
 	for(int i=1;i<n;i++){
-		//cout<<B[i]<<" "<<C[i]<<endl;
-		anss=max(anss,B[i]+C[i]);
+		C[i]=max(C[i],C[i-2]-2);
 	}
-	//cout<<endl;
+	for(int i=1;i<n;i++){
+		if(B[i] && C[i]) anss=max(anss,B[i]+C[i]);
+	}
 	cout<<anss<<"\n";
 	return 0;
 }
